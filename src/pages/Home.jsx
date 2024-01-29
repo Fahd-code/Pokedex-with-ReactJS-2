1a061bce12acbf083d080b77a089567ee9943b2b
Fahd-code <abdelazizbahjaji@gmail.com>
2024-01-29 18:03:18 +0100

    Admin Dashboard

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+// ** React Imports
+import React from 'react';
+
+// ** Custom Components Imports
+import { Header, Listing, Navigation, Search } from '../components'
+
+const Home1 = () => {
+
+    return (
+        <>
+            <Header />
+            <Search />
+            <Navigation />
+            <Listing />
+        </>
+    )
+}
+
+export default Home1
